62750398230a230e806fbd100d7be35b1e333ec8
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-06-28 19:17:48 -0300

    Aula dia 27/06/2022

---
+++ Facec.Minha.Primeira.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Facec.Minha.Primeira.Api.Controllers
+{
+    [Route("hello")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok("Hello world!!!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
